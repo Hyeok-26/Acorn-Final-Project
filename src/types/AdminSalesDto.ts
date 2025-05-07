df84e8554628d000a01955ef1f36253a21352fbc
JiSeon <jsseong0416@gmail.com>
2025-05-07 12:19:29 +0900

    지점 수익 리스트(리스트, 추가, 수정,삭제),  수익 통계 보기 화면(네비바 없음)

---
+++ src/types/AdminSalesDto.ts
@@ -0,0 +1,20 @@
+export interface AdminSalesDto {
+    adminSaleId: number;
+    storeName: string;
+    saleName: string;
+    creDate: string;
+    editDate: string;
+    price: number;
+    cdAcode: string;
+    cdBcode: string;
+    checkedItems: string[];
+    auto: string;
+    startRowNum: number;
+    endRowNum: number;
+    pageNum: number;
+    list: AdminSalesDto[]; // 재귀 구조
+    startPageNum: number;
+    endPageNum: number;
+    totalPageCount: number;
+    totalRow: number;
+  }
