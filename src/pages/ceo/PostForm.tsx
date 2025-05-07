b2fc3075082b08eb1f2e764d127b2dc72126e1c2
Noh Hyeokcheon <makdeng0132@gmail.com>
2025-05-07 17:38:57 +0900

    코드/품목/지점/공지 페이지 파일 생성 (미완성성)- 혁천

---
+++ src/pages/ceo/PostForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PostForm(props) {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    );
+}
+
+export default PostForm;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,25 @@ import Home from "../pages/Home"
 import Admin from "../pages/admin/Admin";
 import Ceo from "../pages/ceo/Ceo";
 import App from "../App";
+import Code from "../pages/ceo/Code";
+import Product from "../pages/ceo/Product";
+import Store from "../pages/ceo/Store";
+import Post from "../pages/ceo/Post";
+import PostDetail from "../pages/ceo/PostDetail";
+import PostUpdateForm from "../pages/ceo/PostUpdateForm";
+import PostForm from "../pages/ceo/PostForm";
 
 const routes = [
     {path: '/', element: <Home />},
     {path: '/admin', element: <Admin />},
     {path: '/ceo', element: <Ceo />},
+    {path: '/code', element:<Code/>},
+    {path: '/product', element:<Product/>},
+    {path: '/store', element:<Store/>},
+    {path: '/posts', element:<Post/>},
+    {path: '/posts/new', element:<PostForm/>},
+    {path: '/posts/:postId', element:<PostDetail/>},
+    {path: '/posts/:postId/edit', element:<PostUpdateForm/>}
 ]
 
 const router = createBrowserRouter([{
