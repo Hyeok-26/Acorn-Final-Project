3dcfbe7d5c9144b93bd2c77ed9dfd72d2020f224
KWAK <glasofk12345@naver.com>
2025-05-08 18:04:13 +0900

    곽래희_본사발주주

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,18 @@ import Home from "../pages/Home"
 import Admin from "../pages/admin/Admin";
 import Ceo from "../pages/ceo/Ceo";
 import App from "../App";
+import Order from "../pages/ceo/Order";
+import OrderDetail from "../pages/ceo/OrderDetail";
 
 const routes = [
     {path: '/', element: <Home />},
     {path: '/admin', element: <Admin />},
     {path: '/ceo', element: <Ceo />},
+    {path: '/ceo/orders', element: <Order />},
+    {path: '/ceo/orders/:orderId/detail', element: <OrderDetail/>},
+    
+    
+
 ]
 
 const router = createBrowserRouter([{
