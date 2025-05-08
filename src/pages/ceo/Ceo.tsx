76a1df165a430650aeebc471711b3e98eae4ef29
kimtaewoo <ktw2000111@naver.com>
2025-05-08 09:45:47 +0900

    navbar

--- src/pages/ceo/Ceo.tsx
+++ src/pages/ceo/Ceo.tsx
@@ -1,9 +1,13 @@
+import CeoNavBar from "@/components/CeoNavBar";
 
 function Ceo() {
     return (
-        <>
-          <h1>CEO 대시보드 페이지</h1>  
-        </>
+        <div className="flex">
+            <CeoNavBar />
+            <main className="flex-1 p-8">
+                <h1>CEO 대시보드 페이지</h1>
+            </main>
+        </div>
     );
 }
 
