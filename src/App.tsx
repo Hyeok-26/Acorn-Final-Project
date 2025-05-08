73b5bdbc0ac48e65ffbcbdb80ecc7caf6aac79c8
kimtaewoo <ktw2000111@naver.com>
2025-05-08 18:01:56 +0900

    AdminNavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useOutlet, useLocation } from "react-router-dom";
 import CeoNavBar from "@/components/CeoNavBar";
+import AdminNavBar from "@/components/AdminNavBar";
 
 function App() {
   const currentOutlet = useOutlet();
   const location = useLocation();
-  const isCeoPage = location.pathname.includes('/ceo') || 
-                   location.pathname.includes('/code') || 
-                   location.pathname.includes('/store');
+  const isCeoPage = location.pathname.includes('/ceo');
+  const isAdminPage = location.pathname.includes('/admin');
 
   return (
     <div className="flex">
       {isCeoPage && <CeoNavBar />}
+      {isAdminPage && <AdminNavBar />}
       <div className="flex-1">{currentOutlet}</div>
     </div>
   )
