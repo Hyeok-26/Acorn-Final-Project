eee0bc8d6fe224338e97cc68e4e7963c65dfcd3e
Jang hee ju <s6000000@naver.com>
2025-05-09 12:13:55 +0900

    지점_수업_기본셋팅팅

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,13 @@ import Home from "../pages/Home"
 import Admin from "../pages/admin/Admin";
 import Ceo from "../pages/ceo/Ceo";
 import App from "../App";
+import Class from "../pages/admin/Class";
 
 const routes = [
     {path: '/', element: <Home />},
     {path: '/admin', element: <Admin />},
     {path: '/ceo', element: <Ceo />},
+    {path:"/admin/class", element:<Class/>}
 ]
 
 const router = createBrowserRouter([{
