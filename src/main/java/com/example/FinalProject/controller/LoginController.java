bb9841ee25d4796fffa060d3daf38685f3cfb7e7
kimtaewoo <ktw2000111@naver.com>
2025-05-11 17:26:09 +0900

    로그인 처리 끝!

--- src/main/java/com/example/FinalProject/controller/LoginController.java
+++ src/main/java/com/example/FinalProject/controller/LoginController.java
@@ -1,8 +1,8 @@
 package com.example.FinalProject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FinalProject.dto.LoginDto;
@@ -13,13 +13,8 @@ public class LoginController {
 	@Autowired private LoginService loginService;
 
 	//품목 리스트
-	@GetMapping("/login")
-    public LoginDto list( @RequestParam(required = true) String userId,
-    		 @RequestParam(required = true) String userPw) {
-     
-		LoginDto loginDto = new LoginDto();
-		loginDto.setId(userId);
-		loginDto.setPw(userPw);
-		return loginService.login(loginDto);
-    }
+	@PostMapping("/login")
+	public LoginDto list(@RequestBody LoginDto loginDto) {
+	    return loginService.login(loginDto);
+	}
 }
